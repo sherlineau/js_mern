4123fbe9d6612792cae546b639118cbf020f4d3c
Sherline Au <au.sherline@gmail.com>
2022-07-06 22:56:44 -0500

    object master assignment

---
+++ JavaScript/Fundamentals/objectMaster.js
@@ -0,0 +1,60 @@
+const pokémon = Object.freeze([
+        { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+        { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+        { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+        { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+        { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+        { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+        { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+        { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+        { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+        { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+        { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+        { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+        { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+        { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+        { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+        { "id": 98,  "name": "Krabby",     "types": ["water"] },
+        { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+        { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+        { "id": 133, "name": "Eevee",      "types": ["normal"] },
+        { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+        { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+        { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+        { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+    ]);
+
+const bListPkmn = pokémon.filter( p => p.name[0] === "B" );
+const pkmnIds = pokémon.map( p => p.id )
+
+// array of pokemon objects where id is divisible by 3
+const id3 = pokémon.filter( p => p.id % 3 === 0);
+console.log(id3)
+
+// array of pokemon object tahat are fire tpes
+const fireTypes = pokémon.filter ( p => p.types.includes("fire") );
+console.log(fireTypes)
+
+// array of objecs that have more than one type 
+const multiType = pokémon.filter ( p => p.types.length > 1);
+console.log(multiType)
+
+//return just a array of pokemon names
+const pokeNames = pokémon.map ( p => p.name )
+console.log(pokeNames)
+
+//return names of id greater than 99
+const id99 = pokémon.filter( p => p.id > 99).map( p => p.name)
+console.log(id99)
+
+//return names of pokemon with just poison type
+const poisonTypes = pokémon.filter( p => p.types.length < 2 && p.types.includes('poison')).map( p => p.name)
+console.log(poisonTypes)
+
+// return first type of all pokemon whose second type is flying
+const flying = pokémon.filter( p => p.types.includes("flying")).map( p => p.types[0])
+console.log(flying)
+
+//return count of nummber with normal type
+const normalType = pokémon.filter( p => p.types.includes("normal"))
+console.log(normalType.length)
