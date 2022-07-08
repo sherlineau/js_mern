ca5a3dae084eb2bf361a44303bc8625c4a7d7dec
Sherline Au <au.sherline@gmail.com>
2022-07-08 14:03:21 -0500

    changed person card from class comp to functional

--- React/person-card/src/App.js
+++ React/person-card/src/App.js
@@ -4,10 +4,10 @@ import PersonCardComponent from './components/PersonCardComponent';
 function App() {
   return (
     <div className="App">
-      <PersonCardComponent firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
-      <PersonCardComponent firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
-      <PersonCardComponent firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
-      <PersonCardComponent firstName="Maria" lastName="Smith" age={52} hairColor="Brown"/>
+      <PersonCardComponent firstName="Jane" lastName="Doe" a={45} hairColor="Black"/>
+      <PersonCardComponent firstName="John" lastName="Smith" a={88} hairColor="Brown"/>
+      <PersonCardComponent firstName="Millard" lastName="Fillmore" a={50} hairColor="Brown"/>
+      <PersonCardComponent firstName="Maria" lastName="Smith" a={52} hairColor="Brown"/>
     </div>
   );
 }
--- React/person-card/src/components/PersonCardComponent.jsx
+++ React/person-card/src/components/PersonCardComponent.jsx
@@ -1,34 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-export default class PersonCardComponent extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      age: props.age
-    }
+const PersonCardComponent = (props) => {
+
+  const {firstName, lastName, a, hairColor} = props
+  const [age, setAge] = useState(a)
+
+  const addAge = ()=> {
+    setAge(age+1)
   }
 
-  render() {
-    //destructure prop
-    //props are an "empty" object created by the Component parent class
-    //key names are passed in through App.js [firstName = "asdfasf"] => {firstName: "asdfasf"}
-    const {firstName, lastName, age, hairColor} = this.props
-
-    // function to set state
-    const addAge = ()=> {
-      this.setState(
-        {
-          age : this.state.age + 1
-        }
-      )
-    }
-    return (
-      <div>
-        <h1>{lastName}, {firstName}</h1>
-        <p>Age: {this.state.age} </p>
-        <p>Hair Color: {hairColor}</p>
-        <button onClick={addAge}>Birthday Button for {firstName} {lastName}</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+    <h1>{lastName}, {firstName}</h1>
+    <p>Age: {age} </p>
+    <p>Hair Color: {hairColor}</p>
+    <button onClick={addAge}>Birthday Button for {firstName} {lastName}</button>
+  </div>
+  )
 }
+
+export default PersonCardComponent
