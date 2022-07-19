016c19dbf2cf8632a67ef4ea7e2645cab051b212
Sherline Au <au.sherline@gmail.com>
2022-07-19 12:40:39 -0500

    full stack demo

---
+++ demos/fullstack/fullstack1-demo/client/src/views/Details.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom"
+
+// 1. get id from params (useParams)
+// 2. display info on load (useEffect)
+// 3. for the info: useState
+// 4. grab info from backend : axios
+
+const Details = () => {
+    const [song, setSong] = useState()
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/songs/${id}`)
+            .then(res => setSong(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:8000/api/songs/${id}`)
+        .then(res => navigate('/'))
+        .catch(err => console.log(err))
+    }
+
+    return (
+        <fieldset>
+            <legend> Details.jsx</legend>
+            {
+                song ?
+                    <div>
+                        <h5> Title: {song.title}</h5>
+                        <h5> Artist: {song.artist}</h5>
+                        <h5> Rating: {song.rating}</h5>
+                        <button onClick={handleDelete}>Delete</button>
+                    </div> :
+                    <h1> The song is not available</h1>
+
+            }
+
+        </fieldset>
+    )
+}
+
+export default Details
